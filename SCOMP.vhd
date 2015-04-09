e81268336d8620f0f5baada0221c1567324728b9
Alec Fenichel <fenichelar@gatech.edu>
2015-04-09 13:13:16 -0400

    Create starting assembly file

--- SCOMP.vhd
+++ SCOMP.vhd
@@ -89,7 +89,7 @@ BEGIN
 		wrcontrol_aclr_a => "NONE",
 		address_aclr_a   => "NONE",
 		outdata_aclr_a   => "NONE",
-		init_file        => "SimpleRobotProgram.mif",
+		init_file        => "DemoProgram.mif",
 		lpm_hint         => "ENABLE_RUNTIME_MOD=NO",
 		lpm_type         => "altsyncram"
 	)
